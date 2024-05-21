117be9460653c53d883feaf982ed43dbc62719e0
knockknock <yangban11111@gmail.com>
2024-05-21 18:28:18 +0900

    Add: 아웃렛 콜렉션 설정

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     @IBOutlet var BoldLabel: UILabel!
     @IBOutlet var regularLabel: UILabel!
     @IBOutlet var imageView: UIImageView!
+    
+    
+    @IBOutlet var questionLabels: [UILabel]!
+    
     @IBOutlet var heightLabel: UILabel!
     @IBOutlet var heightView: UIView!
     @IBOutlet var heightTextField: UITextField!
@@ -23,9 +27,28 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        boldLabelUI()
+        regularLabelUI()
+        questionLabelUI()
     }
-
-
+    
+    private func boldLabelUI() {
+        BoldLabel.text = "BMI Calculator"
+        BoldLabel.textAlignment = .left
+        BoldLabel.font = UIFont.boldSystemFont(ofSize: 25)
+    }
+    
+    private func regularLabelUI() {
+        regularLabel.text = "당신의 BMI 지수를\n알려드릴게요."
+        regularLabel.numberOfLines = 2
+        regularLabel.textAlignment = .left
+        regularLabel.font = UIFont.systemFont(ofSize: 18)
+    }
+    
+    private func questionLabelUI() {
+        heightLabel.text = "키가 어떻게 되시나요?"
+        heightLabel.font = UIFont.systemFont(ofSize: 18)
+        heightLabel.textAlignment = .left
+    }
 }
 
