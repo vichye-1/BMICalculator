a7a9380e81a63dd95a294be235675751485164ef
knockknock <yangban11111@gmail.com>
2024-05-21 18:09:28 +0900

    Add: 스토리보드 제약사항 / 아웃렛 연결

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var BoldLabel: UILabel!
+    @IBOutlet var regularLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var heightView: UIView!
+    @IBOutlet var heightTextField: UITextField!
+    @IBOutlet var weightLabel: UILabel!
+    @IBOutlet var weightView: UIView!
+    @IBOutlet var weightTextField: UITextField!
+    @IBOutlet var randomButton: UIButton!
+    @IBOutlet var resultButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
